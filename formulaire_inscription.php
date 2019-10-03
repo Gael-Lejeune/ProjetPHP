61e5ceae0303824b752bc8250b865f9cfc270ddd
b17037314 <laetitia.bourges@etu.univ-amu.fr>
2019-10-03 15:11:03 +0200

    affichage message erreur en cas de mauvais valeurs des champs

--- formulaire_inscription.php
+++ formulaire_inscription.php
@@ -3,9 +3,16 @@ include "utils.inc.php";
 
 start_page('Inscription');
 
+$step=$_GET['step'];
+if ($step == 'ERROR_mdp') {
+    echo 'La confirmation de mot de passe est incorrecte'.'<br/>';
+} else if ($step == 'ERROR_cond') {
+    echo 'Vous devez valider les conditions d\'utilisations'.'<br/>';
+}
+
 ?>
 
-    <hr/><br/><strong>Formulaire dInscription</strong><br/><hr/>
+    <hr/><br/><strong>Formulaire d'inscription</strong><br/><hr/>
     <form action="data-processor.php" method="post">
         <p> Votre Nom </p>
         <input type="text" name="name"/>
@@ -13,12 +20,12 @@ start_page('Inscription');
         <input type="radio" name="civilite" value="M"/> Homme <br/>
         <br><input type="radio" name="civilite" value="F"/> Femme <br/>
         <p> Email </p>
-        <input type="text" name="email"/>
+        <input type="email" name="email"/>
         <p> Mot de Passe </p>
         <input type="password" name="password"/>
         <p> Confirmation Mot de Passe </p>
-        <input type="password" name="password"/></br></br>
-        <p>Conditions générales <input type="checkbox" name="conditions"/></p>
+        <input type="password" name="passwordconf"/></br></br>
+        <p>Conditions générales <input type="checkbox" name="conditions" value="ok"/></p>
         <input type="submit" name="action" value="OK"/>
     </form>
 <?php
