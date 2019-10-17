8b17411ee44bff7984398bac5957522d1b862dd8
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-10-17 12:18:39 +0200

    début pagination (qui marche pas encore), BD acytualisée pour messages et discussions

--- controller/test_chat.php
+++ controller/test_chat.php
@@ -7,17 +7,19 @@ include '../model/dtb.inc.php';
 start_page("login", "html/css/signup.css", "stylesheet", "fonts.googleapis.com/css?family=Oswald&display=swap", "stylesheet");
 
 ?>
-<a class="arrow" href="<?php echo $indexaddr ?>"><img src="https://img.icons8.com/nolan/50/000000/up-left.png" style="margin-top: -20px;"></a>
+<a class="arrow" href="<?php echo $indexcontroller ?>"><img src="https://img.icons8.com/nolan/50/000000/up-left.png" style="margin-top: -20px;"></a>
 
 <div class='Title' style="margin-top: 20px;">
     <div> <img alt="Logo" src="<?php echo $logo ?>"> </div>
-    <div class="FreeNote highlightTextIn"> <a alt="FreeNote" href="<?php echo $indexaddr ?>"> FreeNote </a> </div>
+    <div class="FreeNote highlightTextIn"> <a alt="FreeNote" href="<?php echo $indexcontroller ?>"> FreeNote </a> </div>
 </div>
 
 <h1> Discussion </h1>
 
 <div class="container-form">
     <form class="form" action="<?php echo $test_chat_processing ?>" method="post">
+        <p>Votre nom</p>
+        <input class="bouton" name="name"  type="text" required placeholder="Rentrez votre nom ici">
         <p> Votre message (un mot entre 1 et 15 caractères)</p>
         <input class="bouton" type="text" name="texte" minlength="1" maxlength="15" required placeholder="Rentrez votre message ici"/>
         <input class="submit" type="submit" name="action" value="Envoyer"/>
--- model/test_chat-processing.php
+++ model/test_chat-processing.php
@@ -6,27 +6,95 @@ include '../model/dtb.inc.php';
 
 session_start();
 
-dtbconnect();
+$dbLink = dtbconnect();
+
+//vérif de l'ID du message
+if (isset($_POST['id_msg']))
+{
+    $id_msg=$_POST['id_msg'];
+}
+else{
+    $id_msg=" ";
+}
+
+//vérif de celui qui poste le message
+if (isset($_POST['name']))
+{
+    $name=$_POST['name'];
+}
+else{
+    $name=" ";
+}
+
+//vérif de l'ID de la discussion
+if (isset($_POST['id_discussion']))
+{
+    $id_discussion=$_POST['id_discussion'];
+}
+else{
+    $id_discussion=" ";
+}
+
+//vérif si la discussion est ouverte ou non
+if (isset($_POST['est_ouvert']))
+{
+    $est_ouvert=$_POST['est_ouvert'];
+}
+else{
+    $est_ouvert=" ";
+}
+
+//vérif du mot écrit
+if (isset($_POST['texte']))
+{
+    $texte=$_POST['texte'];
+}
+else{
+    $texte=" ";
+}
 
 //Recuperation des variables en post
 $id_msg=($_POST['id_msg']);
-$user_msg=($_POST['user_msg']);
+$user_msg=($_POST['name']);
 $id_discussion=($_POST['id_discussion']);
 $est_ouvert=($_POST['est_ouvert']);
 $texte=($_POST['texte']);
+$action=($_POST['action']);
 
 //Definition de la requete mySql
-$query="INSERT INTO message (texte) VALUES ('$texte')";
+//$query="INSERT INTO message (id_msg, user_msg, id_discussion, est_ouvert, texte) VALUES ('$id_msg','$user_msg','$id_discussion','$est_ouvert','$texte')";
+$query="INSERT INTO message(id_msg, message.name, id_discussion, est_ouvert, texte)
+        SELECT id_msg, message.name, id_discussion,est_ouvert,texte
+        FROM user JOIN message ON user.name = message.name
+        WHERE est_ouvert IS TRUE
+        ORDER BY id_msg DESC LIMIT 0,10";
+
+var_dump();
 
 //Verification de la viabilité de la requete
-$dbResult = querycheck($dbLink, $query, 'Requete viable');
+$dbResult = querycheck($dbLink, $query);
+
+var_dump();
 
 //lit le résultat de la requête dans un tableau associatif
 $dbRow=mysqli_fetch_assoc($dbResult);
-echo $dbRow;
 
-//redirection HTTP vers test_chat.php
+if ($est_ouvert == TRUE || $action='action') {
+    //ouverture variables de session
+    $_SESSION['id_msg']=$id_msg;
+    $_SESSION['user_msg']=$user_msg;
+    $_SESSION['id_discussion']=$id_discussion;
+    $_SESSION['est_ouvert']=$est_ouvert;
+    $_SESSION['texte']=$texte;
 
-header('Location: test_chat.php?step=ERROR_mdp');
+    //On demarre la session
+    header("Location:$indexcontroller");
+
+}
+else {
+    //Erreur
+    header("Location:$logincontroller?error=ERROR");
+}
+
 
 ?>
